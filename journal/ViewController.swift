0badb6193a201d0d0c61bf6fe3be800b204f8b51
Miguel Tomairo <matomairo@gmail.com>
2020-04-10 12:28:03 -0500

    Initial Commit

---
+++ journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  journal
+//
+//  Created by miguel tomairo on 4/10/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
